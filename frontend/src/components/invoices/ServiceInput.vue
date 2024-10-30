ccdf3d2e967963f3b6601faef9a768e00e918182
Mikita Slabysh <slabysh2015@gmail.com>
2024-10-30 18:05:23 -0400

    Improved layout

--- frontend/src/components/invoices/ServiceInput.vue
+++ frontend/src/components/invoices/ServiceInput.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="border-round border-3 border-300 w-7 h-6rem mt-3 surface-overlay font-bold flex">
 
-    <div id="top-row" class="w-full flex"> <!--w-1 h-2rem flex-->
-      <InputText type="text" placeholder="Description" class="w-7 mr-1 h-2rem"/>
-      <InputText type="text" placeholder="Rate" size="small" class="w-2 mr-1 h-2rem"/>
-      <InputText type="text" placeholder="Quantity" size="small" class="w-2 mr-1 h-2rem"/>
-      <InputText type="text" placeholder="Tax" size="small" class="w-2 mr-1 h-2rem"/>
-    </div>
+    <div>
+      <div id="top-row" class="w-full flex mt-1"> <!--w-1 h-2rem flex-->
+        <InputText type="text" placeholder="Title" class="w-5 mr-1 h-2rem"/>
+        <InputText type="text" placeholder="Rate" size="small" class="w-2 mr-1 h-2rem"/>
+        <InputText type="text" placeholder="Quantity" size="small" class="w-2 mr-1 h-2rem"/>
+        <InputText type="text" placeholder="Tax" size="small" class="w-1 mr-1 h-2rem"/>
+        <InputText type="text" placeholder="Total" size="small" class="w-2 mr-1 h-2rem"/>
+      </div>
 
+      <div id="bottom-row" class="w-full mt-1 flex">
+        <InputText type="text" placeholder="Description" size="small" class="w-12 mr-1 h-3rem"/>
+      </div>
+    </div>
+    
 
 
     
